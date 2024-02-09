853ecb7c40c4e908309416c3f92249df9ba3614c
shirin <test@gmail.com>
2024-02-08 19:06:45 -0500

    my_projects

---
+++ names_screen/names_model.dart
@@ -0,0 +1,12 @@
+//
+// import 'package:hive/hive.dart';
+//
+// part 'names_model.g.dart';
+//
+// @HiveType(typeId: 1)
+// class NamesModel{
+//   @HiveField(0)
+//   final String name;
+//
+//   NamesModel({required this.name});
+// }
